b3539341e37533bdf82b5e06a5505620afd8c15f
Andrey_Soin <soinproduction@gmail.com>
2022-06-13 21:46:22 +0300

    bug fix

--- source/js/common.js
+++ source/js/common.js
@@ -97,6 +97,61 @@ const stickyHeaderFunction = () => {
 };
 
 
+const getAccordions = function(dataName = "[data-id]") {
+  return document.querySelectorAll(dataName);
+}
+
+const accordions = getAccordions();
+let openedAccordion = null;
+
+const closeAccordion = function (accordion) {
+  accordion.style.maxHeight = 0;
+  accordion.classList.remove('open')
+};
+
+const openAccordion = function (accordion) {
+  accordion.style.maxHeight = accordion.scrollHeight + "px";
+  accordion.classList.add('open')
+};
+
+const toggleAccordionButton = function (button, className = "active") {
+  button.classList.toggle(className);
+};
+
+const checkIsAccordionOpen = function(accordion) {
+  return accordion.style.maxHeight && accordion.style.maxHeight !== "0px";
+}
+
+const accordionClickHandler = function () {
+
+  let curentDataNumber = this.dataset.id
+
+  toggleAccordionButton(this);
+
+  const accordionContent = document.querySelector(`[data-content="${curentDataNumber}"]`);
+  const isAccordionOpen = checkIsAccordionOpen(accordionContent);
+
+  if (isAccordionOpen) {
+    closeAccordion(accordionContent);
+    openedAccordion = null;
+  } else {
+    if (openedAccordion != null) {
+      closeAccordion(openedAccordion);
+      const accordionButton = document.querySelector(`[data-id="${openedAccordion.dataset.content}"]`)
+      toggleAccordionButton(accordionButton);
+    }
+    openAccordion(accordionContent);
+    openedAccordion = accordionContent;
+  }
+}
+
+const activateAccordion = function(accordions, handler) {
+  for (const accordion of accordions) {
+    accordion.addEventListener('click', handler)
+  }
+}
+activateAccordion(accordions,accordionClickHandler);
+
 
 
 
